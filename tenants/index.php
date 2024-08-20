fd138f046040a4b9b7950b2b5211c5b085f9057c
Henry James Ribano <henryjamesribano27@gmail.com>
2024-08-20 22:29:55 +0800

    day fckin 9

--- tenants/index.php
+++ tenants/index.php
@@ -1,7 +1,7 @@
 <?php
 include 'includes/header.php';
 ?>
-<nav class="shadow py-2 sticky top-0 px-5 md:px-[120px]  md:flex items-center justify-between bg-background ">
+<nav class="shadow py-2 z-20 sticky top-0 px-5 md:px-[120px]  md:flex items-center justify-between bg-background ">
     <div class="flex justify-between items-center">
         <img src="../assets/img/poblacionease.png" alt="logo" class="w-[150px]">
         <span class="text-3xl cursor-pointer md:hidden block">
@@ -20,35 +20,40 @@ include 'includes/header.php';
         <li class="my-6 md:my-0 ">
             <a href="#about" class="text-lg hover:text-primary duration-500">Find Agent</a>
         </li>
-        <a class="profile-name md:hidden flex items-center hover:text-primary duration-500" href="profile">
+        <a class="profile-name md:hidden flex items-center hover:text-primary duration-50 ease-in transition-colors" href="profile">
             <img src="../assets/img/me.jpg" alt="profile-picture" class="cursor-pointer rounded-full" style="width: 50px; height: 50px; margin-right: 20px;">
             <p class="text-lg " href="#">John Doe</p>
         </a>
     </ul>
-    <div class="profile-name md:flex hidden md:items-center">
+    <a class="profile-name md:flex hidden md:items-center hover:text-primary duration-50 ease-in transition-colors" href="profile">
         <img src="../assets/img/me.jpg" alt="profile-picture" class=" rounded-full" style="width: 50px; height: 50px; margin-right: 20px;">
-        <a class="text-lg hover:text-primary duration-500" href="#">John Doe</a>
-    </div>
+        <p class="text-lg hover:text-primary duration-500" href="#">John Doe</p>
+    </a>
 </nav>
-<section class="relative h-[500px] z-0" style="background-image: url('../assets/img/bg.png');  background-size: cover; z-index: -2;">
-    <div class="absolute inset-0 bg-black opacity-50"></div>
-    <div class="relative container mx-auto flex flex-col justify-center items-center text-center h-full">
-        <h1 class="text-4xl font-bold">Find your next home that suits you</h1>
-        <p class="mt-4  mb-4">Easy to use. Convenient. All in one place.</p>
+<section class="relative h-[500px] tenants-home" style="background-image: url('../assets/img/bg.png'); z-index: 0; background-size: cover; background-position: center;">
+    <div class="container mx-auto flex flex-col justify-center items-center text-center h-full px-4 text-[20px]">
+        <h1 class="text-4xl font-bold">Discover a New Era of Convenience and Connection</h1>
+        <p class="mt-4 mb-4 text-base md:text-lg">Experience effortless living in Poblacion, Taal with Poblacion<span class="text-primary">Ease</span>.</p>
 
-        <div class="mt-10 border border-primary md:px-0 py-3 md:py-6 px-3   " style="border: 1px solid black;">
-            <select class="bg-white text-black  rounded border select-input">
-                <option>Choose Location</option>
-                <!-- Add more options here -->
-            </select>
-            <select class="bg-white text-black py-2 rounded select-input">
-                <option>Long Term Rentals</option>
-                <!-- Add more options here -->
-            </select>
-            <button class="bg-primary  px-6 py-2 rounded-full text-white">Search</button>
+        <div class="flex flex-col md:flex-row items-center gap-4 mt-4 w-full max-w-xl">
+            <div class="w-full">
+                <select class="shadow px-3 py-3 w-full md:w-[100px] rounded-lg md:rounded-l-lg">
+                    <option value="">Choose Barangay</option>
+                </select>
+            </div>
+            <div class="w-full">
+                <select class="shadow px-3 py-3 w-full md:w-[100px] rounded-lg md:rounded-l-lg">
+                    <option value="">Choose Rental Term</option>
+                    <option value="">Long Term Rentals</option>
+                    <option value="">Short Term Rentals</option>
+                    <option value="">Daily Rentals</option>
+                </select>
+            </div>
+            <button class="bg-primary text-white p-2 w-full md:w-auto rounded-lg md:rounded-r-lg hover:bg-accent">Search</button>
         </div>
     </div>
 </section>
+
 
 
 <script>
---
+++ tenants/profile.php
@@ -0,0 +1,3 @@
+<?php require 'includes/header.php'; ?>
+<h1>Profile</h1>
+<?php require 'includes/footer.php'; ?>
