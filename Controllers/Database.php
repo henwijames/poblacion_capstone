e714510e1b87b36d9111fc63d1afb1eac30a8436
Henry James Ribano <114987518+henwijames@users.noreply.github.com>
2025-05-15 21:56:06 +0800

    Update Database.php

--- Controllers/Database.php
+++ Controllers/Database.php
@@ -1,19 +1,19 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php'; // Ensure this path is correct
+require_once __DIR__ . '/../vendor/autoload.php'; 
 use Dotenv\Dotenv;
 
-// Specify the path to your .env file
-$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-// Change __DIR__ if your .env is in another directory
 
-// Load the .env file and check for success
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+
+
+
 try {
     $dotenv->load();
 } catch (Exception $e) {
     die('Failed to load .env file: ' . $e->getMessage());
 }
-// Check if the environment variable is set
+
 if (!$_ENV['DB_DATABASE']) {
     die("DB_DATABSE is not set in the environment variables.");
 }
@@ -21,14 +21,13 @@ if (!$_ENV['DB_DATABASE']) {
 class Database
 {
     private $host = "localhost";
-    private $db_name; // Make sure this is set to your database name
-    private $username = "root"; // Your database username
-    private $password = ""; // Your database password
+    private $db_name; 
+    private $username = "root"; 
+    private $password = ""; 
     public $conn;
 
     public function __construct()
     {
-        // Assign the database name from the environment variable
         $this->db_name = $_ENV['DB_DATABASE'];
     }
 
